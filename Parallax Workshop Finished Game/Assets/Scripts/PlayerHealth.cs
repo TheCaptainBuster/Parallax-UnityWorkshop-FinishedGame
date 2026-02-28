c66d2f00d04b7b249127e4eb3f44fa7244cd6f0e
TheCaptainBuster <thecaptainbuster.dev@gmail.com>
2026-02-28 18:29:53 +0530

    Functional HealthBar and Playerhealth system

--- Parallax Workshop Finished Game/Assets/Scripts/PlayerHealth.cs
+++ Parallax Workshop Finished Game/Assets/Scripts/PlayerHealth.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    public HealthBar healthBarScript;
+    private void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBarScript.SetMaxHealth(maxHealth);
+    }
+    private void Update()
+    {
+        //action due to which player will lose health
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void TakeDamage (int damageAmt)
     {
-        
+        currentHealth -= damageAmt;
+        healthBarScript.SetPlayerHealth(currentHealth);
     }
 }
